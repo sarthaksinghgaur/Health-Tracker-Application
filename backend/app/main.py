97f837ad7d855d58d79bafb62497dc37f78fe04a
Sarthak Singh Gaur <s7s5g4@gmail.com>
2025-08-05 10:27:32 +0530

    fix cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from .services import calc_bmr, sum_nutrients, parse_webhook
 
 app = FastAPI(title="Professional Health Tracker")
 
-origins = ["http://127.0.0.1:5500", "http://localhost:5500"]
+origins = ["http://127.0.0.1:8080", "http://localhost:8080"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
