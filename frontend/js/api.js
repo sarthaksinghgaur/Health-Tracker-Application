3d12a6c5655b51ae4beed7aff38d5797af388c84
Sarthak Singh Gaur <s7s5g4@gmail.com>
2025-08-05 13:22:55 +0530

    prepare for vercel deployment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = '/api';
+// const BASE_URL = 'http://127.0.0.1:8000';
 
 export async function loginUser(data) {
   const res = await fetch(`${BASE_URL}/login`, {
